658da86ea9f75f56f283f7c76cb5a51dbe97b5d8
s1f102601799 <s1f102601799@iniad.org>
2026-06-28 23:47:26 +0900

    perfect(almost)
    構造はほぼ完成。デザインなどはあとで調整

--- nagoya/nagoya_travel.js
+++ nagoya/nagoya_travel.js
@@ -4,14 +4,19 @@ const next = document.querySelector(".next");
 
 let current = 0;
 
-// 画像を表示
+// 画像の○を自動表示
 function showSlide(index){
 
-    slides.forEach(slide=>{
+    slides.forEach(slide =>{
         slide.classList.remove("active");
     });
 
+    dots.forEach(dot =>{
+        dot.classList.remove("active");
+    });
+
     slides[index].classList.add("active");
+    dots[index].classList.add("active");
 }
 
 // 次へ
@@ -52,3 +57,35 @@ setInterval(function(){
     showSlide(current);
 
 },3000);
+//　画像の○をクリックしたらその画像を表示する
+const dotsContainer = document.querySelector(".dots");
+
+slides.forEach((slide, index) => {
+    const dot = document.createElement("span");
+    dot.classList.add("dot");
+
+    if(index === 0){
+        dot.classList.add("active");
+    }
+
+    dot.addEventListener("click", () => {
+        current = index;
+        showSlide(current);
+    });
+
+    dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".dot");
+
+dots.forEach((dot,index)=>{
+
+    dot.addEventListener("click",()=>{
+
+        current = index;
+
+        showSlide(current);
+
+    });
+
+});
